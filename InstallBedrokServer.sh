85339c8f53e3db113f5b02c401bb186056ea1358
MilesGlitch <59733060+MilesGlitch@users.noreply.github.com>
2022-03-22 23:13:54 +0300

    Add files via upload

---
+++ InstallBedrokServer.sh
@@ -0,0 +1,6 @@
+apt install git
+echo --- Instaling BedrokServer ---
+git clone https://github.com/MilesGlitch/BedrokServerTranslator.git
+cd BedrokServerTranslator
+chmod +x installTermux.sh
+./installTermux.sh
