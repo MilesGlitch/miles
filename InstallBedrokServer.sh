c1f910dfd06020aa3cc0eebea30aaa12399bb8eb
Miles <59733060+MilesGlitch@users.noreply.github.com>
2022-06-21 23:31:50 +0300

    Delete InstallBedrokServer.sh

--- InstallBedrokServer.sh
+++
@@ -1,6 +0,0 @@
-apt install git -y
-echo --- Instaling BedrokServer ---
-git clone https://github.com/MilesGlitch/BedrokServerTranslator.git
-cd BedrokServerTranslator
-chmod +x installTermux.sh
-./installTermux.sh
